aeaf8202b950f90ed78613c96bbac562f37eef73
Alex <jineriko@mail.ru>
2024-02-05 20:36:29 +0300

    Создан файл lessons.properties в пакете resoursec. Изменен код в Lessons01, добавлен новый вывод на экран

--- src/lesson01/Lesson01.java
+++ src/lesson01/Lesson01.java
@@ -3,5 +3,7 @@ package lesson01;
 public class Lesson01 {
     public static void main(String[] args) {
         System.out.print("Lesson01");
+        System.out.println("---------");
+        System.out.println("Lesson01 edit");
     }
 }
