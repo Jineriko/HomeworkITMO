c351c58609b1d3017e24c3413ad5ea4e613fa25d
Alex <jineriko@mail.ru>
2024-02-17 21:20:07 +0300

    Домашнее задание к 5 уроку

---
+++ src/homework/lesson05/Task1.java
@@ -0,0 +1,28 @@
+package homework.lesson05;
+
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+         * Отсортировать массив пузырьковой сортировкой
+         * */
+        int[] array = {-4, 55, 924, -41, 0, -100, 58};
+        System.out.println("Массив до сортировки \n" + Arrays.toString(array));
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                int temp;
+                if (array[i - 1] > array[i]) {
+                    temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+        System.out.println("Массив после сортировки \n" + Arrays.toString(array));
+    }
+}
---
+++ src/homework/lesson05/Task2.java
@@ -0,0 +1,21 @@
+package homework.lesson05;
+
+public class Task2 {
+    public static void main(String[] args) {
+        /*
+        * Дан массив целых чисел: int[] task02 = {88, 34, 12, 55, 90, 4, 10, 44}.
+        * Найти максимальную сумму четырёх смежных целых чисел в массиве task02.
+        * Использовать алгоритм скользящего окна.
+        * */
+        int[] task02 = {88, 34, 12, 55, 90, 4, 10, 44};
+        int maxSum = 0;
+
+        for (int i = 3; i < task02.length; i++) {
+            int tempSum = task02[i - 3] + task02 [i - 2] + task02 [i - 1] + task02 [i];
+
+            if (tempSum > maxSum)
+                maxSum = tempSum;
+        }
+        System.out.println("Максимальная сумма четырёх смежных чисел = " + maxSum);
+    }
+}
