28a2b4d29db1d53c9908725b3c27887267389de9
Jineriko <158499473+Jineriko@users.noreply.github.com>
2024-06-04 10:02:23 +0300

    Delete src/com/company/project/lesson01 directory

--- src/com/company/project/lesson01/Lesson01.java
+++
@@ -1,9 +0,0 @@
-package com.company.project.lesson01;
-
-public class Lesson01 {
-    public static void main(String[] args) {
-        System.out.print("Lesson01");
-        System.out.println("---------");
-        System.out.println("Lesson01 edit");
-    }
-}
--- src/com/company/project/lesson01/NewClass.java
+++
@@ -1,7 +0,0 @@
-package com.company.project.lesson01;
-
-public class NewClass {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
